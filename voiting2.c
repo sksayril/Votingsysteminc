58ab73583bb41a0ff5602a6b697f5155503dec73
Sk Sayril Amed <116861849+sksayril@users.noreply.github.com>
2022-12-25 11:20:15 +0530

    Voting Sytem 2

--- voiting2.c
+++ voiting2.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 #include<string.h>
-int check,bjp=0,app=0,cpi=0,cancelvote=0;
-char id[10],pass[10],new_id[10],new_pass[10];
-void bjpvote();
-void appvote();
-void cpivote();
+int bjp=0,app=0,cpi=0,cancelvote=0,resultbj,resultapp,resultcpi;
+char ch;
+char id[10],pass[10],new_id[10],new_pass[10],id_1[10],pass_1[10];
 void create();
 void login();
+void voting();
+void result();
+void login2();
 void main()
 {
 create();
@@ -23,16 +24,71 @@ void create(){
 
 }
 void login(){
-char id_1[10],pass_1[10];
 printf("Enter Your id:");
 scanf("%s",&id_1);
 printf("Enter Your Password");
 scanf("%s",&pass_1);
 if (strcmp(id_1,id)==0 && strcmp(pass_1,pass)==0)
 {
-    printf("Welcome");
+    voting();
+    
 }
 else{
     printf("Invalid id pass");
 }
 }
+
+void voting(){
+     printf("\nEnter 1 to bjp");
+     printf("\nEnter 2 to app");
+     printf("\nEnter 3 To cpi");
+     printf("\nEnter 4 To Admin Login For check Result\n");
+    scanf("%s",&ch);  
+    switch (ch)
+    {
+    case '1':
+       bjp++;
+       resultbj=bjp++;
+       voting();
+        break;
+    case '2':
+    app++;
+    resultapp=app++;
+    voting();
+    break;
+    case '3':
+    cpi++;
+    resultcpi=cpi++;
+    voting();
+    break;
+    case '4':
+    login2();
+    break;
+    default:
+    cancelvote++;
+    printf("Thankyou");
+    voting();
+        break;
+    }  
+    
+    
+    }
+    void result(){
+        printf("Total Vote APP%d\n",resultapp);
+        printf("Total Vote BJP%d\n",resultbj);
+        printf("Total Vote Cpi%d\n",resultcpi);
+
+    }
+    void login2(){
+    printf("Enter Your id:");
+    scanf("%s",&id_1);
+    printf("Enter Your Password");
+    scanf("%s",&pass_1);
+    if (strcmp(id_1,id)==0 && strcmp(pass_1,pass)==0)
+    {
+        result();        
+    }
+    else{
+        printf("Access Denied");
+    }
+    }
